cf09ebdb3ec3b3e4cc41326b312c193363515dd8
Mustafa <mus@musalbas.com>
2014-11-27 19:00:54 +0000

    Method for getting indicator data for all countries (untested)

--- app/src/main/java/uk/ac/kcl/worldstatus/app/backend/WorldBankData.java
+++ app/src/main/java/uk/ac/kcl/worldstatus/app/backend/WorldBankData.java
@@ -10,6 +10,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * An API for the World Bank data sets.
@@ -23,8 +24,9 @@ public class WorldBankData {
      * @param indicator The name of a development indicator.
      * @return An ArrayList of [x, y] data points.
      */
-    public static ArrayList<Float[]> getIndicatorData(String country, String indicator, int fromYear, int toYear) throws IOException, JSONException, ParserConfigurationException, SAXException {
-        String XMLString = Utils.getDataFromURL("http://api.worldbank.org/countries/" + country + "/indicators/" + indicator + "?per_page=100&date=" + fromYear + ":" + toYear + "&format=xml");
+    public static ArrayList<Float[]> getIndicatorDataByCountry(String country, String indicator, int fromYear, int toYear) throws IOException, JSONException, ParserConfigurationException, SAXException {
+        String XMLString = Utils.getDataFromURL("http://api.worldbank.org/countries/" + country + "/indicators/" + indicator + "?per_page=1000&date=" + fromYear + ":" + toYear + "&format=xml");
+
         XMLString = XMLString.substring(3);
         Document document = Utils.getDocumentFromString(XMLString);
 
@@ -52,9 +54,44 @@ public class WorldBankData {
         return dataPoints;
     }
 
+    /**
+     * Gets indicator data about all countries for a specific year.
+     * @param indicator The name of the development indicator.
+     * @param year The year.
+     * @return A country, value hashmap.
+     */
+    public static HashMap<String, Float> getIndicatorDataByYear(String indicator, int year) throws IOException, SAXException, ParserConfigurationException {
+        String XMLString = Utils.getDataFromURL("http://api.worldbank.org/countries/all/indicators/" + indicator + "?per_page=1000&date=" + year + "&format=xml");
+
+        XMLString = XMLString.substring(3);
+        Document document = Utils.getDocumentFromString(XMLString);
+
+        NodeList nodeList = document.getElementsByTagName("wb:data");
+
+        HashMap<String, Float> data = new HashMap<String, Float>();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+
+                String value = element.getElementsByTagName("wb:value").item(0).getTextContent();
+                String country = element.getElementsByTagName("wb:country").item(0).getTextContent();
+
+                if (!value.equals("")) {
+                    Float valueFloat = Float.parseFloat(value);
+                    data.put(country, valueFloat);
+                }
+            }
+        }
+
+        return data;
+    }
+
     // TODO remove this main method
     public static void main(String[] args) throws IOException, JSONException, ParserConfigurationException, SAXException {
-        ArrayList<Float[]> tempFloats = getIndicatorData("USA", "AG.LND.ARBL.ZS", 1960, 2014);
+        ArrayList<Float[]> tempFloats = getIndicatorDataByCountry("USA", "AG.LND.ARBL.ZS", 1960, 2014);
 
         for (int a = 0; a < tempFloats.size(); ++a) {
             System.out.println(tempFloats.get(a)[0]);
