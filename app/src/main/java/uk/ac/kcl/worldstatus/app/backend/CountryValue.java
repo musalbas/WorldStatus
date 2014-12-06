293cba247a5325b1e232436cb45ab36b4c3875fb
Mustafa <mus@musalbas.com>
2014-12-06 18:48:45 +0000

    Clean up CountryValue.java

--- app/src/main/java/uk/ac/kcl/worldstatus/app/backend/CountryValue.java
+++ app/src/main/java/uk/ac/kcl/worldstatus/app/backend/CountryValue.java
@@ -1,35 +1,60 @@
 package uk.ac.kcl.worldstatus.app.backend;
 
 /**
- * @author Harrison Perry
- *         A class which holds the value of a country i.e. how favourable it is to the user.
- *         Higher is better.
+ * A class which holds the value of a country i.e. how favourable it is to the user.
+ * Higher is better.
  */
 public class CountryValue {
 
-    public String name;
-    public float val;
-    public float score = 0;
+    private String name;
+    private float val;
+    private float score = 0;
 
+    /**
+     * Initialise the country value.
+     *
+     * @param name Name of the country.
+     * @param val  Value of the country.
+     */
     public CountryValue(String name, float val) {
         this.name = name;
         this.val = val;
 
     }
 
+    /**
+     * Get the name of the country.
+     *
+     * @return The name of the country.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get the value of the country.
+     *
+     * @return The value of the country.
+     */
     public float getVal() {
         return val;
 
     }
 
+    /**
+     * Get the score of the country.
+     *
+     * @return The score of the country.
+     */
     public float getScore() {
         return score;
     }
 
+    /**
+     * Increment the score of the country.
+     *
+     * @param score The score to increment by.
+     */
     public void setScore(float score) {
         this.score = score + this.score;
     }
