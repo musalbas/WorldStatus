d7d8421b4ee4f2ea03a7bed106687c8cc3377259
Mustafa <mus@musalbas.com>
2014-12-07 14:30:31 +0000

    Fix cancel bug

--- app/src/main/java/uk/ac/kcl/worldstatus/app/backend/Utils.java
+++ app/src/main/java/uk/ac/kcl/worldstatus/app/backend/Utils.java
@@ -15,7 +15,7 @@ import java.net.URL;
  */
 public class Utils {
 
-    private static boolean cancelled = true;
+    private static boolean cancelled = false;
 
     /**
      * Gets data from an URL.
